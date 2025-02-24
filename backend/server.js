fd78e8256ffc37eed87694cfa9d7db3983b2742d
phewphewer <joshvvidal23@gmail.com>
2025-02-24 08:11:06 +0800

    Update server.js
    changed listen for request message more clarification

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,10 @@ app.use("/api/workouts", workoutRoutes);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
+
     // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log("Connected on DB . . Listening on port ", process.env.PORT);
+      console.log("Connected to Database . . Listening on port ", process.env.PORT);
     });
   })
   .catch((error) => {
